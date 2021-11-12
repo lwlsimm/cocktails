4c302172235c26926bac20296b3976d4246c2112
lwlsimm <77008736+lwlsimm@users.noreply.github.com>
2021-11-12 14:32:22 +0000

    deploy

--- resources/views/cocktailView.blade.php
+++ resources/views/cocktailView.blade.php
@@ -20,7 +20,7 @@ function goBack() {
   <div class="card mb-3 cocktail-display " style="max-width: 100%;">
   <div class="row g-0">
     <div class="col-md-4">
-      <img src={{$photo}} class="img-fluid rounded-start" alt="{{$name}}">
+      <img id="cocktailImg" src={{$photo}} class="img-fluid rounded-start" alt="{{$name}}">
     </div>
     <div class="col-md-8">
       <div class="card-body text-center">
